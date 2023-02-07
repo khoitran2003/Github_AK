41354f8fcac7669cb3e160c999fa85d91de2dd42
khoitran2003 <anhkhoi246813579@gmail.com>
2023-02-07 19:03:38 +0700

    Thêm 1 dòng code nữa nha

---
+++ baitap1-100.py
@@ -0,0 +1,21 @@
+# f = open('inp.txt', 'r')
+# data = f.readlines()
+# f.close()
+# f = open('out.txt', 'w')
+
+f = open('inp.txt', 'r')
+data = f.readlines()
+f.close()
+f = open('out.txt', 'w')
+
+  
+    
+
+
+
+
+
+
+
+
+    
